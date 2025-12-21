e311a339d2d580551d3ce5d3b7ed1d3780a36348
muhmedalmkkawy <mohamed.almkkawy@mazayasolutions.com>
2025-12-21 09:14:44 +0200

    first upload the frontend of cms dashboard

---
+++ app/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page projects_page">
+    <div class="container">
+      <div class="image mazaya_logo">
+        <img src="../assets/images/logo.png" alt="logo image" loading="lazy">
+      </div>
+      <div class="projects_content">
+        <div class="project_item">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { useApiAsyncData } from '@/composables/useApiFetch'
+
+  const { data } = useApiAsyncData('products', 'https://dummyjson.com/products' , false , true);
+</script>
+
+<style lang="scss" scoped>
+  .projects_page{
+    min-height: 100vh;
+    .container{
+      @include displayFlex();
+    }
+  }
+</style>
